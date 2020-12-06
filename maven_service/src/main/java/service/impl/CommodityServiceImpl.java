c5b3fa2739830bc0af96d04edbcd211ed0859c4b
HelloHJC <1092938015@qq.com>
2020-12-06 23:55:44 +0800

    Modify Commodity Service

--- maven_service/src/main/java/service/impl/CommodityServiceImpl.java
+++ maven_service/src/main/java/service/impl/CommodityServiceImpl.java
@@ -1,2 +1,12 @@
-package service.impl;public class CommodityServiceImpl {
+package service.impl;
+
+import po.Commodity;
+import service.CommodityService;
+
+import java.util.List;
+
+public class CommodityServiceImpl implements CommodityService {
+    public List<Commodity> selectAll(Commodity commodity) {
+        return null;
+    }
 }
